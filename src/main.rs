26ff285ae5b87c07118a7cc44f059b70108d616e
= <=>
2024-06-16 11:42:21 +0100

    Added vec3 and operators

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,156 @@
-use std::f64;
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::{f64, ops};
 use std::fs::File;
 use std::io::prelude::*;
+use std::ops::{Div, Mul};
 use std::path::Path;
+
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0/rhs)
+    }
+}
+
+impl ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3::new(
+            self.x / rhs,
+            self.y / rhs,
+            self.z / rhs
+        )
+    }
+}
+
+impl Vec3 {
+    fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    fn length_squared(&self) -> f64 {
+        (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
+    }
+
+    fn dot(&self, rhs: Vec3) -> f64 {
+        (self.x * rhs.x)
+        + (self.y * rhs.y)
+        + (self.z * rhs.z)
+    }
+
+    fn cross(&self, rhs: Vec3) -> Vec3 {
+        Vec3::new(
+            self.y * rhs.z - self.z * rhs.y,
+            self.z * rhs.x - self.x * rhs.z,
+            self.x * rhs.y - self.y * rhs.x
+        )
+    }
+
+    fn unit_vector(&self) -> Vec3{
+        self / self.length()
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3::new(-self.x, -self.y, -self.z)
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self.x + rhs.x,
+            self.y + rhs.y,
+            self.z + rhs.z
+        )
+    }
+}
+impl ops::AddAssign<Vec3> for Vec3 {
+    fn add_assign(&mut self, rhs: Vec3) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self.x - rhs.x,
+            self.y - rhs.y,
+            self.z - rhs.z,
+        )
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self.x * rhs.x,
+            self.y * rhs.y,
+            self.z * rhs.z,
+        )
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3::new(
+            self.x * rhs,
+            self.y * rhs,
+            self.z * rhs
+        )
+    }
+}
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self * rhs.x,
+            self * rhs.y,
+            self * rhs.z
+        )
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3{
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+    }
+}
+
 
 fn main() {
     // Image
