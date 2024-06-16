54feab0fc44895ec1a9d6e0ad53571de826cd9b7
= <=>
2024-06-16 14:07:16 +0100

    Chapter 4.2 - Casting ray into scene

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,15 @@ use std::io::prelude::*;
 use std::ops::{Div, Mul};
 use std::path::Path;
 
+#[derive(Copy, Clone)]
 struct Vec3 {
     x: f64,
     y: f64,
     z: f64
 }
 
-impl ops::Div<f64> for Vec3 {
-    type Output = Vec3;
-
-    fn div(self, rhs: f64) -> Self::Output {
-        self * (1.0/rhs)
-    }
-}
-
-impl ops::Div<f64> for &Vec3 {
-    type Output = Vec3;
-
-    fn div(self, rhs: f64) -> Self::Output {
-        Vec3::new(
-            self.x / rhs,
-            self.y / rhs,
-            self.z / rhs
-        )
-    }
-}
+type Color = Vec3;
+type Point = Vec3;
 
 impl Vec3 {
     fn new(x: f64, y: f64, z: f64) -> Vec3 {
@@ -143,6 +127,24 @@ impl ops::MulAssign<f64> for Vec3 {
     }
 }
 
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0/rhs)
+    }
+}
+impl ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3::new(
+            self.x / rhs,
+            self.y / rhs,
+            self.z / rhs
+        )
+    }
+}
 impl ops::DivAssign<f64> for Vec3{
     fn div_assign(&mut self, rhs: f64) {
         self.x /= rhs;
@@ -151,25 +153,73 @@ impl ops::DivAssign<f64> for Vec3{
     }
 }
 
+struct Ray {
+    origin: Point,
+    direction: Vec3
+}
+impl Ray {
+    fn new(origin: Point, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    fn at(&self, t: f64) -> Point {
+        self.origin + (t * self.direction)
+    }
+}
 
 fn main() {
+    let aspect_ratio = 16.0 / 9.0;
     // Image
-    let width: i32 = 256;
-    let height: i32 = 256;
+    let width: f64 = 400.00;
+    let height: f64 = width / aspect_ratio;
 
-    println!("P3\n{} {}\n255\n", width, height);
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * f64::from(width/height);
+    let camera_center = Point::new(0.0, 0.0, 0.0);
 
-    for h in 0..height {
-        for w in 0..width {
-            let r :f64 = f64::from(w) / (width as f64 - 1.0);
-            let g :f64 = f64::from(h) / (height as f64 - 1.0);
-            let b :f64 = 0.0;
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+    let pixel_delta_u = viewport_u / width;
+    let pixel_delta_v = viewport_v / height;
 
-            print!("{} {} {}\n", ir, ig, ib);
+    let viewport_upper_left = camera_center
+                                    - Vec3::new(0.0, 0.0, focal_length)
+                                    - viewport_u/2.0
+                                    - viewport_v/2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    write_image_header(width, height);
+
+    for h in 0..height as i32 {
+        for w in 0..width as i32 {
+            let pixel_center = pixel00_loc
+                                        + (f64::from(w) * pixel_delta_u)
+                                        + (f64::from(h) * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let ray: Ray = Ray::new(camera_center, ray_direction);
+
+            let color: Color = ray_color(ray);
+            write_color(color);
         }
     }
+}
+
+fn ray_color(ray: Ray) -> Color {
+    let unit_direction = ray.direction.unit_vector();
+    let a = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - a)* Color::new(1.0, 1.0, 1.0)
+        + a * Color::new(0.5, 0.7, 1.0)
+}
+fn write_image_header(width: f64, height: f64) {
+    print!("P3\n{} {}\n255\n", width as i32, height as i32);
+}
+
+fn write_color(color: Color) {
+    let ir = (255.999 * color.x) as i32;
+    let ig = (255.999 * color.y) as i32;
+    let ib = (255.999 * color.z) as i32;
+
+    print!("{} {} {}\n", ir, ig, ib);
 }
